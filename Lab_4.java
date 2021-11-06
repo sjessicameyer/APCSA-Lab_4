52e90297daffaa4c0419aa0151f0f84bd3f14b77
Sar2x <60858973+sjessicameyer@users.noreply.github.com>
2021-11-06 01:41:42 +0000

    All fixed!

--- Lab_4.java
+++ Lab_4.java
@@ -6,10 +6,7 @@
 		Purpose: To gain proficiency implementing recursive methods in Java.
 */
 
-import SarahLibrary.*;
-
 public class Lab_4{
-  SarahLibrary.customMath customMath = new SarahLibrary.customMath();
   public int twos(int num){
     //returns the number of factors of 2 in num
     if (num%2!=1){
@@ -30,11 +27,9 @@ public class Lab_4{
 
   public void reverse(int num){
     //displays the result of reversing num's digits
-    int currentDigit=customMath.findDigit(num,1);
-    System.out.print(currentDigit);
-    int newNum= (num-currentDigit)/10;
-    if (newNum!=0){
-      reverse(newNum);
+    if(num!=0){
+      System.out.print(num % 10);
+      reverse(num / 10);
     }
   }
 
@@ -49,25 +44,13 @@ public class Lab_4{
   public void printWithCommas(int num){
     //displays num with commas inserted properly
 
-    /*if %3 equals 0 or 2 (100.len%3=0, 10000.len%3=2), print first num
-      if %3 equals 1 (1000.len%3=1), print first num then comma
-      if %3 equals 2 (1000.len%3=2),
-    */
-    int firstNum=num%(customMath.power(10, customMath.length(num)));
-    int leftoverNum=num-firstNum*(int)customMath.power(10, customMath.countLength(num)-1);
-    customMath.
-    if ((customMath.countLength(num)%3==1)&&(leftoverNum!=0)){
-      System.out.print(firstNum);
-      System.out.print(",");
-      printWithCommas(leftoverNum);
-    }else if (leftoverNum==0){
-      System.out.print(firstNum);
-    }else if (num!=0){
-      //TO DO: Make it so numbers ending in 0 still print correctly
-      System.out.print(firstNum);
-      printWithCommas(leftoverNum);
+    if(num < 1000){
+      System.out.print(num);
     }
-
+    else {
+      printWithCommas(num / 1000);
+      System.out.print("," + (num % 1000) / 100 + "" + (num % 100) / 10 + "" + num % 10);
+    }
 
   }
 
